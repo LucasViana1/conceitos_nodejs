8e899f7a749ea9b34f463824ee6ed4bf80530b5e
Lucas Viana <lucasviana112@gmail.com>
2020-06-16 22:21:53 -0300

    conclusão desafio conceitos nodejs

--- src/app.js
+++ src/app.js
@@ -1,33 +1,86 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  // const techsArray = techs.split(",").map((tech) => tech.trim());
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  if (!isUuid(id)) return response.status(400).json({ message: "Invalid id" });
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0)
+    return response.status(400).json({ message: "Repository not found" });
+
+  repositories[index] = {
+    ...repositories[index],
+    title,
+    url,
+    techs,
+  };
+
+  return response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index < 0)
+    return response.status(400).json({ message: "Repository not found" });
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: "Invalid id" });
+  }
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  repositories[index] = {
+    ...repositories[index],
+    likes: repositories[index].likes + 1,
+  };
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
